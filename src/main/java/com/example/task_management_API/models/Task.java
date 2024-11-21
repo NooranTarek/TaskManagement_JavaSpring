1ac91d916c18b3349292d26074915bc17491bab8
Nouran Tarek <noort.mohamed@gmail.com>
2024-11-21 16:43:28 +0200

    using lombok instead of manual setters and getters

--- src/main/java/com/example/task_management_API/models/Task.java
+++ src/main/java/com/example/task_management_API/models/Task.java
@@ -1,10 +1,11 @@
 package com.example.task_management_API.models;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDate;
 
-
+@Data
 @Entity
 @Table(name = "tasks")
 public class Task {
@@ -19,55 +20,6 @@ public class Task {
     private Status status;
     @Enumerated(EnumType.STRING)
     private Periority periority;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(LocalDate dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-    public Periority getPeriority() {
-        return periority;
-    }
-
-    public void setPeriority(Periority periority) {
-        this.periority = periority;
-    }
-
     public enum Status{
         to_do,
         in_progress,
