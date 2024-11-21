b6baf740addef8f6e36cc21649cfba4478cfb178
Nouran Tarek <noort.mohamed@gmail.com>
2024-11-21 14:15:23 +0200

    creating model of task

---
+++ src/main/java/com/example/task_management_API/models/Task.java
@@ -0,0 +1,82 @@
+package com.example.task_management_API.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue (strategy=GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    @Enumerated(EnumType.STRING)
+    private Periority periority;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Periority getPeriority() {
+        return periority;
+    }
+
+    public void setPeriority(Periority periority) {
+        this.periority = periority;
+    }
+
+    public enum Status{
+        to_do,
+        in_progress,
+        completed
+    }
+    public enum Periority{
+        low,
+        medium,
+        high
+    }
+
+}
